9dae4c1b429631a97b50f84b4f8aa05cb8f8100a
Martin Brueggemann <marty.brueggemann@Martins-MacBook-Pro.local>
2021-04-18 13:09:50 -0500

    Through Array methods

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const zooAnimals = [
   displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(){
+  function animalNames() {
     let displayNames = [];
     zooAnimals.forEach(function(item) {
       displayNames.push(`name: ${item.animal_name}, scientific: ${item.scientific_name}`);
@@ -74,20 +74,23 @@ const zooAnimals = [
   For example: ['jackal, asiatic', .....]
   */
 
-  function lowerCaseNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowerCaseNames(array) {
+    let lowerNames = array.map(x => x["animal_name"].toLowerCase())
+    return lowerNames;
   }
-  
+  lowerCaseNames(zooAnimals);
   
   /* 🦁🦁🦁 Request 3: .filter() 🦁🦁🦁
   The zoo is concerned about animals with a lower population count. 
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowPopulationAnimals(array) {
+    let newArray = array.filter(x=> x["population"] < 5)
+    return newArray;
   }
   
+lowPopulationAnimals(zooAnimals);
 
   /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
   The zoo needs to know their total animal population across the United States. 
@@ -95,10 +98,12 @@ const zooAnimals = [
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
+  function USApop(array) {
+    let total = array.reduce((accum, item) => accum + item["population"],0)
+    return total
   }
   
+  USApop(zooAnimals);
   
   // 🦁🦁🦁 Callbacks 🦁🦁🦁  
   /* 🦁🦁🦁 Step 1: Create a higher-order function 🦁🦁🦁
